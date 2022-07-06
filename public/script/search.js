ba214150d56bfb2746fdddc7b74d41589bef8d38
SorenWi <soerenwinterhalder@gmail.com>
2022-07-06 18:14:53 +0200

    css cleanup + different way of creating cards in html

--- public/script/search.js
+++ public/script/search.js
@@ -3,6 +3,8 @@ searchBar = document.getElementById("searchBar");
 fuelType = document.getElementById("searchFuelType");
 searchBtn = document.getElementById("searchBtn");
 
+cardTemplate = document.getElementById("cardTemplate").innerHTML;
+
 searchBtn.addEventListener("click", search);
 
 console.log("search js working");
@@ -25,52 +27,22 @@ async function search() {
 
 function showResults(results) {
   //clear previous search results
+  //https://stackoverflow.com/questions/16270761/how-to-insert-a-large-block-of-html-in-javascript
   resultsDiv.innerHTML = "";
   for (let i = 0; i < results.length; i++) {
+    card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = cardTemplate;
+    card.innerHTML = card.innerHTML
+      .replace(/{NAME}/g, results[i].carName)
+      .replace(/{ID}/g, results[i].carId)
+      .replace(/{FUELTYPE}/g, results[i].fuelType)
+      .replace(/{TIMEFRAME}/g, `${results[i].earlyTime} - ${results[i].lateTime}`)
+      .replace(/{MAXTIME}/g, results[i].maxTime)
+      .replace(/{FLATRATE}/g, results[i].flatrate)
+      .replace(/{COSTPERMINUTE}/g, results[i].costPerMinute);
 
-    resultCard = document.createElement("div");
-    resultCard.classList.add("card");
-
-    resultCard.innerHTML = `
-      <div class="cardHeader">
-        <h3 class="cardHeading">${results[i].carName}</h3>
-        <p class="cardSubheading">ID: ${results[i].carId}</p>
-      </div>
-      <h3 class="cardSectionHeading">Information</h3>
-      <div class="cardSectionGrid3">
-        <div>
-          <h3 class="cardInfoHeading">Fuel Type</h3>
-          <p>${results[i].fuelType}</p>
-        </div>
-        <div>
-          <h3 class="cardInfoHeading">Time</h3>
-          <p>Time Frame: ${results[i].earlyTime} - ${results[i].lateTime}</p>
-          <p>Max Time: ${results[i].maxTime}</p>
-        </div>
-        <div>
-          <h3 class="cardInfoHeading">Cost</h3>
-          <p>Flatrate: ${results[i].flatrate}</p>
-          <p>Cost per minute: ${results[i].costPerMinute}</p>
-        </div>
-      </div>
-
-      <h3 class="cardSectionHeading">Check Availability / Book</h3>
-      <div class="cardSectionGrid2">
-        <label for="date">Date</label>
-        <input type="date" name="date">
-        <label for="time">Time</label>
-        <input type="time" name="time">
-        <label for="duration">Duration</label>
-        <input type="number" name="duration">
-
-        <button>Check Availability</button>
-        <button>Book</button>
-      </div>
-    `;
-
-    resultsDiv.append(resultCard);
-
-    //event listener on buttons
+    resultsDiv.append(card);
 
     console.log(results[i].carName)
   }
