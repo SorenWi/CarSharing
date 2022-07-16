213cbdd0e0760bfe0249154214d2c11b99cdefde
SorenWi <soerenwinterhalder@gmail.com>
2022-07-16 17:22:13 +0200

    added admin

--- script/server.js
+++ script/server.js
@@ -1,3 +1,4 @@
+//Imports
 const express = require('express')
 const app = express()
 const session = require("express-session")
@@ -10,6 +11,7 @@ const UserModel = require("../models/User.js");
 const CarModel = require("../models/Car.js");
 const BookingModel = require("../models/Booking.js");
 
+//Express setup
 const port = 3000;
 const mongoURI = "mongodb://localhost:27017/CarSharing";
 
@@ -24,6 +26,10 @@ mongoose.connect(mongoURI).then((res) => {
   console.log("MongoDB connected");
 });
 
+
+// -- Authentification
+// --- Authentification Setup
+
 const store = new MongoDBSession({
   uri: mongoURI,
   collection: "sessions"
@@ -35,6 +41,8 @@ app.use(session({
   saveUninitialized: false,
   store: store
 }))
+
+// --- Authentification Middleware
 
 const isAuth = (req, res, next) => {
   if (req.session.isAuth) {
@@ -60,6 +68,8 @@ const ifAuthRedirectToProfile = (req, res, next) => {
   }
 }
 
+// -- Routing / GET requests
+
 app.get("/", async (req, res) => {
   res.render("search", {isAuth: req.session.isAuth, isAdmin: req.session.isAdmin});
 });
@@ -81,6 +91,8 @@ app.get("/profile", isAuth, async (req, res) => {
   const costInfo = await getUserCostInfo(req.session.userId);
   res.render("profile", { username: user.username, isAuth: req.session.isAuth, totalCost: costInfo.totalCost, averageCost: costInfo.averageCost.toFixed(2), totalBookings: costInfo.totalBookings, isAdmin: req.session.isAdmin});
 });
+
+// --- Authentification POST requests / login and register
 
 app.post("/register", async (req, res) => { 
   const { username, password } = req.body;
@@ -107,7 +119,6 @@ app.post("/register", async (req, res) => {
   console.log("created new user:", username)
   res.redirect("/login")
 })
-
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body
@@ -138,7 +149,7 @@ app.post("/logout", (req, res) => {
   console.log("logged out user");
 });
 
-
+// -- Adding cars as admin
 app.post("/admin", async (req, res) => {
   const {carId, carName, fuelType, earlyTime, lateTime, maxTime, flatrate, costPerMinute } = req.body;
 
@@ -189,6 +200,7 @@ app.post("/admin", async (req, res) => {
   res.render("admin", response);
 });
 
+// -- Search POST request, handles search and filter requests, responds with results
 const resultsPerSearch = 10;
 app.post("/search", async (req, res) => {
 
@@ -196,11 +208,11 @@ app.post("/search", async (req, res) => {
 
   if (!useFilter) {
     if (showAll) {
-      let results = await CarModel.find().skip(index).limit(resultsPerSearch);
+      const results = await CarModel.find().skip(index).limit(resultsPerSearch);
       res.json({ results: results, index: index + resultsPerSearch });
     } else {
       const { searchText, fuelType } = req.body;
-      let results = await CarModel.find({name: {$regex: `.*${searchText}.*`, $options: 'i'}, fuelType: fuelType}).skip(index).limit(resultsPerSearch);
+      const results = await CarModel.find({name: {$regex: `.*${searchText}.*`, $options: 'i'}, fuelType: fuelType}).skip(index).limit(resultsPerSearch);
       res.json({ results: results, index: index + resultsPerSearch });
     }
   } else {
@@ -235,11 +247,12 @@ app.post("/search", async (req, res) => {
   }
 });
 
+// -- Availability POST request - responds with if car is available
 app.post("/checkAvailability", async (req, res) => {
   const {carId, date, time, duration} = req.body;
   
-  car = await CarModel.findOne({carId: carId}); 
-  price = await getCarPrice(carId, duration);
+  const car = await CarModel.findOne({carId: carId}); 
+  const price = await getCarPrice(carId, duration);
 
   const availableResponseMessage = `The car is available, the cost for ${duration} minutes is ${price}€`;
   const unavailableResponseMessage = `The car is not available for the specified time, the cost for ${duration} minutes would be ${price}€`;
@@ -251,6 +264,7 @@ app.post("/checkAvailability", async (req, res) => {
   }
 });
 
+// -- Booking POST request - books a car if inputs are valid
 app.post("/bookCar", async (req, res) => {
   const {carId, date, time, duration} = req.body;
 
@@ -276,6 +290,7 @@ app.post("/bookCar", async (req, res) => {
   res.json({ message: "Successfully booked car"})
 });
 
+// -- responds with all bookings of user that made the request, divided into past and upcoming bookings
 app.post("/getBookings", async (req, res) => {
   bookings = await BookingModel.find({userId: req.session.userId});
   
@@ -293,10 +308,12 @@ app.post("/getBookings", async (req, res) => {
   res.json({ pastBookings: pastBookings, upcomingBookings: upcomingBookings });
 });
 
+// -- Helper functions
+
 //Returns if a car is available for the specified time
 async function isAvailable(carId, date, rentTime, rentDuration) {
-  alreadyExisting = await BookingModel.find({carId: carId, date: date});
-  car = await CarModel.findOne({carId: carId}); 
+  const alreadyExisting = await BookingModel.find({carId: carId, date: date});
+  const car = await CarModel.findOne({carId: carId}); 
 
   //Check if duration is legit
   if(rentDuration > car.maxTime) {
@@ -324,6 +341,7 @@ async function isAvailable(carId, date, rentTime, rentDuration) {
   return true;
 }
 
+//returns average and total cost of all bookings of user
 async function getUserCostInfo(userId) {
   bookings = await BookingModel.find({userId: userId});
 
@@ -340,11 +358,29 @@ async function getUserCostInfo(userId) {
   return { totalCost: totalCost, averageCost: averageCost, totalBookings: totalBookings };
 }
 
+//calculates and return the price of a car for a certain duration
 async function getCarPrice(carId, duration) {
   car = await CarModel.findOne({carId: carId});
   return car.price + car.pricePerMinute * duration;
 }
 
+//Create standard admin profile if it doesnt already exist 
+async function createAdmin() {
+  adminExists = await UserModel.findOne({username: "admin"});
+
+  if (!adminExists) {
+    admin = new UserModel({
+      username: "admin",
+      password: await bcrypt.hash("admin", 10),
+      isAdmin: true
+    });
+
+    await admin.save();
+  }
+}
+
+//Server starts listening
 app.listen(port, () => {
   console.log('Server running on http://localhost:' + port)
+  createAdmin();
 })
